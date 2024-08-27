3b789a6f00f8503539710d6a85eaa2a502d79405
Aditya Diaz Illyasa <diazillyasa999@gmail.com>
2024-08-27 11:21:28 +0700

    ubah lagi

--- bin/variabel.dart
+++ bin/variabel.dart
@@ -6,4 +6,22 @@ void main() {
   namaSaya = "diaz";
 
   print(namaSaya);
+
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 10;
+  //array2[0] = 30;
+  print(array1);
+  print(array2);
+
+  late var value = asal();
+
+  print("panggil file ini dulu");
+  print(value);
+}
+
+String asal() {
+  print("fungsi asal dipanggil");
+  return "Salatiga";
 }
